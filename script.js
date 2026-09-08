82b97304ba8bbc603bb63833d8734d46a687ec6e
Fanania <fananiae30@gmail.com>
2026-09-08 16:34:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const sources = [
         "meta/loop_vid/video1.mp4",
         "meta/loop_vid/video2.mp4",
-        "meta/loop_vid/video3.mp4"
+        "meta/loop_vid/video3.mp4",
+        "meta/loop_vid/video4.mp4",
+        "meta/loop_vid/video5.mp4"
     ];
 
     let current = 0;
