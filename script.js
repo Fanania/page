5050a16932b3e46a35a7fa3632978145e3c287a6
Fanania <fananiae30@gmail.com>
2026-09-08 14:33:42 +0300

    Create script.js
    added some java script for the 2nd page

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const video = document.getElementById("promoPlaylist");
+    if (!video) return;
+
+    const sources = [
+        "meta/loop_vid/video1.mp4",
+        "meta/loop_vid/video2.mp4",
+        "meta/loop_vid/video3.mp4"
+    ];
+
+    let current = 0;
+
+    function playCurrent() {
+        video.src = sources[current];
+        video.load();
+        video.play();
+    }
+
+    video.addEventListener("ended", function () {
+        current = (current + 1) % sources.length;
+        playCurrent();
+    });
+
+    playCurrent();
+});
