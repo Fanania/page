9090cb1593fc5bc5efb196d993431c26e7ae662f
Fanania <fananiae30@gmail.com>
2026-09-09 15:15:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const sources = [
        // "meta/loop_vid/video1.mp4",
-        "meta/VIDEO-2026-09-03-15-33-58.mp4",
         "meta/VIDEO-2026-09-03-15-33-13.mp4",
         "meta/loop_vid/video2.mp4",
         "meta/VIDEO-2026-09-03-15-33-59 2.mp4",
         "meta/VIDEO-2026-09-03-15-33-53 2.mp4",
         "meta/VIDEO-2026-09-03-16-33-41.mp4",
-        "meta/VIDEO-2026-09-03-15-33-55 2.mp4",
+    //    "meta/VIDEO-2026-09-03-15-33-55 2.mp4",
      //   "meta/loop_vid/video3.mp4",
      //   "meta/loop_vid/video4.mp4",
         "meta/loop_vid/video5.mp4"
+        "meta/VIDEO-2026-09-03-15-33-58.mp4",
     ];
 
     let current = 0;
